3f59a65c985dd2f7b2e9234af29be5fd0301f7ac
Darake <adamson.gert@hotmail.com>
2020-05-11 01:25:33 +0300

    exercise 9.11

--- patientor/src/index.ts
+++ patientor/src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import diagnosesRouter from './routes/diagnoses';
+import patientRouter from './routes/patients';
 
 const app = express();
 
@@ -14,6 +15,7 @@ app.get('/api/ping', (_req, res) => {
 });
 
 app.use('/api/diagnoses', diagnosesRouter);
+app.use('/api/patients', patientRouter);
 
 app.listen(PORT, () => {
   console.log(`App listening on ${PORT}`);
---
+++ patientor/src/services/patientService.ts
@@ -0,0 +1,12 @@
+import patientData from '../../data/patients.json';
+import { Patient, NonSensitivePatient } from '../types';
+
+const patients: Array<Patient> = patientData as Array<Patient>;
+
+const getNonSensitiveEntries = (): Array<NonSensitivePatient> => {
+  return patients.map(({ id, name, dateOfBirth, gender, occupation }) => (
+    { id, name, dateOfBirth, gender, occupation }
+  ));
+};
+
+export default { getNonSensitiveEntries };
